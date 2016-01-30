7937025b1c59fd3661a6ef4b48eb3692cf869432
BeardMage <mesk.utp@gmail.com>
2016-01-30 16:28:38 +0100

    Upload du projet

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector]
+	public bool playersTurn = true;
+
+	private int level = 3;
+
+	void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		boardScript.SetupScene(level);
+	}
+
+	public void GameOver()
+	{
+		enabled = false;
+	}
+}
