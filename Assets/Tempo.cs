eae7a1022e3e415e76f9123a7cf8550fa2db5d6f
Ulkan <camilleleonard.j@gmail.com>
2016-01-31 15:20:16 +0100

    tempo
    tempo

---
+++ Assets/Tempo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tempo : MonoBehaviour {
+
+    public float tempo = 0.65f;
+    public float resize = 2f;
+    float timer = 0;
+
+    Vector3 sizeInit;
+
+	// Use this for initialization
+	void Start () {
+        sizeInit = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (timer > tempo * 0.8f )
+        {
+            transform.localScale = Vector3.Lerp(sizeInit, sizeInit * resize, tempo * 0.2f);
+        }
+        if (timer > tempo)
+        {
+            transform.localScale = sizeInit;
+            timer = 0;
+        }
+
+        transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+
+        timer += Time.deltaTime;
+	}
+}
